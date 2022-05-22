7cd23440a94f48d719a72d80f333f5eead2991c1
twa04175 <twa04175@naver.com>
2022-05-22 20:54:00 +0900

    get Rotate Cube Pos

--- assets/Script/Cube.ts
+++ assets/Script/Cube.ts
@@ -1,6 +1,7 @@
 
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, Prefab, instantiate, Vec3,math } from 'cc';
 const { ccclass, property } = _decorator;
+const { Mat4 } = math;
 
 /**
  * Predefined variables
@@ -13,23 +14,76 @@ const { ccclass, property } = _decorator;
  * ManualUrl = https://docs.cocos.com/creator/3.4/manual/en/
  *
  */
- 
+
+export enum DIRECTION{
+    Horizontal,
+    Vertical
+}
+
 @ccclass('Cube')
 export class Cube extends Component {
-    // [1]
-    // dummy = '';
 
-    // [2]
-    // @property
-    // serializableDummy = 0;
+    @property({type: Prefab})
+    public block: Prefab = null;
+
+    private cubes: Node[][][] = [[[],[],[]],[[],[],[]],[[],[],[]]];
+    private currentCubes: Node[][][] = [[[],[],[]],[[],[],[]],[[],[],[]]];
 
     start () {
-        // [3]
+        for(let x = 0; x<3; x++) {
+            for(let y = 0; y<3; y++) {
+                for(let z = 0; z<3; z++) {
+                    let block: Node = instantiate(this.block);
+                    block.setPosition(new Vec3(1.2*x,1.2*y,1.2*z));
+                    block.parent = this.node;
+                    this.cubes[x][y][z] = block;
+                    this.currentCubes[x][y][z] = block;
+                }
+            }
+        }
+
+        this.schedule(()=>{
+            this.rotate(Math.floor(Math.random()*2), Math.floor(Math.random()*3));
+        }, 1,100);
     }
 
-    // update (deltaTime: number) {
-    //     // [4]
-    // }
+    // 회전이 필요한 노드들의 번호를 가져오는 함수
+
+    /** 방향과 번호가 주어졋을 때 회전연산 실행
+     *  방향과 번호를 통해 회전해야하는 노드들의 위치 참조 가져오기
+     */
+    rotate(dir:DIRECTION, line:Number){
+        console.log('Cube.ts:rotate:53 -> Rotate ',JSON.stringify(dir),':',line,'line');
+
+        let cubes = [];
+        let center = null;
+        for(let x = 0; x<3; x++) {
+            for(let y = 0; y<3; y++) {
+                for(let z = 0; z<3; z++) {
+                    if(dir === DIRECTION.Vertical) {
+                        if(line === z) {
+                            cubes.push(this.currentCubes[x][y][z]);
+                        }
+                    }else if(dir === DIRECTION.Horizontal) {
+                        if(line === y) {
+                            cubes.push(this.currentCubes[x][y][z]);
+                        }
+                    }
+                }
+            }
+        }
+
+        //먼저 가로방향 회전부터
+        for(let i = 0; i<cubes.length; i++) {
+
+        }
+
+        //현재 위치값 전환
+    }
+
+    update (deltaTime: number) {
+        // [4]
+    }
 }
 
 /**
